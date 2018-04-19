dd5d77f1e5731a738cb9230a06a0226912def0a6
Lakeem M <la@lakeems-MacBook-Pro.local>
2018-04-19 08:15:49 -0400

    Initial commit with test marked as todo.

---
+++ 20180418-LM-NYCSchoolsTest/20180418-LM-NYCSchools.swift
@@ -0,0 +1,34 @@
+//
+//  JPCApp.swift
+//  20180418-LM-NYCSchools
+//
+//  Created by lakeem muhammad on 4/18/18.
+//  Copyright © 2018 lakeem muhammad. All rights reserved.
+//
+
+import XCTest
+@testable import JPCApp
+
+class JPCAppTests: XCTestCase {
+    
+    
+
+    override func tearDown() {
+        super.tearDown()
+        
+    }
+
+    var dataSetup : Utilities!
+    var viewModelTest : ViewModel!
+    
+    
+    //TODO: Test School found in SAT List but not in Master List
+    
+    
+    //TODO: Test for error in loading document or document not found
+    
+    
+    //TODO: test parsing and implementation adding to existing dictionary
+    
+
+}
